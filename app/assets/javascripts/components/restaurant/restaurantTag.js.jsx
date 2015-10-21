fc65a3e52b3d0bee9e9826d4b24c0bd289ab5ecb
Austin Kao <austinkao91@gmail.com>
2015-10-21 02:05:24 -0700

    working on tag links

--- app/assets/javascripts/components/restaurant/restaurantTag.js.jsx
+++ app/assets/javascripts/components/restaurant/restaurantTag.js.jsx
@@ -1,4 +1,10 @@
 var RestaurantTagIndex = React.createClass({
+  mixins: [ReactRouter.History],
+  handleClick: function(tag, event) {
+    event.preventDefault();
+    FilterActions.addFilters({tags: [tag]});
+    this.history.pushState(null, "/restaurants");
+  },
   render: function() {
     if(typeof this.props.tags === "undefined") {
         return(
@@ -11,8 +17,8 @@ var RestaurantTagIndex = React.createClass({
         <ul>
           {
             this.props.tags.map(function(tag,idx){
-              return <li key={idx}><a href="#">{tag.title}</a></li>;
-            })
+              return <li className="tag-item" onClick={this.handleClick.bind(null, tag.title)} key={idx}>{tag.title}</li>;
+            }.bind(this))
           }
         </ul>
       );
